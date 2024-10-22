3e2318498d39a750b1890de82fb0e15941fd3846
Vulnorn <mixadrbk31@gmail.com>
2024-10-22 16:12:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewC_2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            int userInput;
+
+            while (isWork)
+            {
+                Console.WriteLine($"Выберите пункт меню:" +
+                    $"1. Сложить два числа." +
+                    $"2. Умножить два числа." +
+                    $"3. Показать рандомное число." +
+                    $"4. Очистить консоль." +
+                    $"5. Выход.");
+
+               userInput = Convert.ToInt32( Console.ReadLine());
+
+                if (userInput == 1)
+                {
+
+                }
+                else if (userInput == 2)
+                {
+                    
+                }
+                else if (userInput == 3)
+                {
+
+                }
+                else if (userInput == 4)
+                {
+
+                }
+                else if (userInput == 5)
+                {
+
+                }
+
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
